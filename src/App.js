d247849811b59db623f31915799ecf3d4b309c08
Oerd Bej <oerdbej@gmail.com>
2023-01-24 18:56:25 +0100

    changed the whole structure of the file, adding some css and some form for the tasks to the main app component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>ToDo list</h1>
+
+      <form autoComplete='off'>
+        <input
+          type='text'
+          name='todos'
+          id='todos'
+          required
+          placeholder='Need to be done'
+        />
+        <button type='submit'>Create</button>
+      </form>
+      <ul>
+        <li>
+          <label htmlFor=''>
+            <input type='checkbox' id='' />
+            Cooking to do
+          </label>
+          <button>Edit</button>
+        </li>
+        <li>
+          <label htmlFor=''>
+            <input type='checkbox' id='' />
+            Cooking to do
+          </label>
+          <button disabled>Edit</button>
+        </li>
+      </ul>
+      <div className='row'>
+        <label htmlFor='all'>
+          <input type='checkbox' name='all' id='all' />
+          ALL
+        </label>
+        <p>You have 0 to do</p>
+        <button id='delete'>Delete</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
