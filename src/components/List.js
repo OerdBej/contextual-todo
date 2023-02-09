b38d96dafb428a97ac7ad779f320f9701b15b10d
Oerd Bej <oerdbej@gmail.com>
2023-02-09 21:47:42 +0100

    adding the functionality for the listitem and the props

--- src/components/List.js
+++ src/components/List.js
@@ -4,10 +4,20 @@ import { DataContext } from "./DataProvider";
 
 const List = () => {
   const [todo, setTodo] = useContext(DataContext);
+
+  const switchComplete = (id) => {
+    alert(id);
+  };
+
   return (
     <ul>
       {todo.map((todo, index) => (
-        <ListItem todo={todo} key={index} id={index} />
+        <ListItem
+          todo={todo}
+          key={index}
+          id={index}
+          checkedComplete={switchComplete}
+        />
       ))}
     </ul>
   );
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,8 +4,9 @@ const ListItem = (props) => {
   return (
     <div>
       <li>
-        <label htmlFor={props.id}>
-          <input type='checkbox' id={props.id} />
+        <label htmlFor={props.id} className={todo.complete ? "active" : ""}>
+          <input type='checkbox' id={props.id} checked={todo.complete} />
+          onchange={() => props.checkComplete(id)} /}
           {props.todo.name}
         </label>
         <button>Edit</button>
